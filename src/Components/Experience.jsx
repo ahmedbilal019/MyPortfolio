0bf24406498cf8a81bb8c68aa7a65c7e91d1f219
ahmedbilal <ahmed258bilal@gmail.com>
2025-06-16 23:17:37 +0500

    adding dummy cards in projects component, adding content in experience component and apply css

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -3,7 +3,24 @@ import React from "react";
 function Experience() {
   return (
     <>
-      <div className="experience-container"> </div>
+      <div className="experience-container">
+        <ul class="list-group">
+          <li href="#" class="list-group-item">
+            <strong>Frontend Development Internship</strong>
+            <br />
+            <p>01-Sep-2024 - 15-Oct-2024</p>
+
+            <small>DevelopersHub Corporation </small>
+          </li>
+          <li href="#" class="list-group-item">
+            <strong>React Development Internship</strong>
+            <br />
+            <p> 23-Nov-2024 - 22-Dec-2024</p>
+
+            <small>CodexCue Software Solutions</small>
+          </li>
+        </ul>
+      </div>
     </>
   );
 }
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -6,16 +6,61 @@ function Projects() {
       <div className="projects-container">
         {" "}
         <div className="project-card">
-          <div className="card" style={{ width: "18rem" }}>
+          <div className="card" style={{ width: "18rem", height: "12rem" }}>
             <img src="..." className="card-img-top" alt="..." />
             <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card’s content.
-              </p>
+              <h5 className="card-title">Project name</h5>
+              <p className="card-text">short description about project</p>
               <a href="#" className="btn btn-primary">
-                Go somewhere
+                Live Preview
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="project-card">
+          <div className="card" style={{ width: "18rem", height: "12rem" }}>
+            <img src="..." className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Project name</h5>
+              <p className="card-text">short description about project</p>
+              <a href="#" className="btn btn-primary">
+                Live Preview
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="project-card">
+          <div className="card" style={{ width: "18rem", height: "12rem" }}>
+            <img src="..." className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Project name</h5>
+              <p className="card-text">short description about project</p>
+              <a href="#" className="btn btn-primary">
+                Live Preview
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="project-card">
+          <div className="card" style={{ width: "18rem", height: "12rem" }}>
+            <img src="..." className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Project name</h5>
+              <p className="card-text">short description about project</p>
+              <a href="#" className="btn btn-primary">
+                Live Preview
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="project-card">
+          <div className="card" style={{ width: "18rem", height: "12rem" }}>
+            <img src="..." className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Project name</h5>
+              <p className="card-text">short description about project</p>
+              <a href="#" className="btn btn-primary">
+                Live Preview
               </a>
             </div>
           </div>
